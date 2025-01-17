63a095baada3444362114883092e3b31c685d336
halidAgha-2000 <mkhalid.k.agha@gmail.com>
2025-01-17 17:50:11 +0200

    Fix GITHUB link, Removing WHATSAPP icon

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React from "react";
 import { BsWhatsapp, BsLinkedin, BsGithub } from "react-icons/bs";
 
 const SocialMedia = () => {
-    return (
-        <div className='app__social'>
-            <a href={'https://github.com/KhalidAgha-2000'} target='_blank' rel='noreferrer'><div> <BsGithub /></div></a>
-            <a href={'https://www.linkedin.com/in/khalid-agha/'} target='_blank' rel='noreferrer'><div><BsLinkedin /></div></a>
-            <a href={'https://wa.me/+96176318263'} target='_blank' rel='noreferrer'><div><BsWhatsapp /></div></a>
+  return (
+    <div className="app__social">
+      <a
+        href={"https://github.com/KHALID-AGHA"}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <div>
+          {" "}
+          <BsGithub />
         </div>
-    )
-}
+      </a>
+      <a
+        href={"https://www.linkedin.com/in/khalid-agha/"}
+        target="_blank"
+        rel="noreferrer"
+      >
+        <div>
+          <BsLinkedin />
+        </div>
+      </a>
+    </div>
+  );
+};
 
-export default SocialMedia
+export default SocialMedia;
